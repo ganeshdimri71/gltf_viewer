86d1d89015a50098fa1e311271aa3eb254b37a08
ganeshdimri71 <ganeshdimri71@gmail.com>
2022-07-28 19:57:49 +0530

    Creating a mesh for plane geometry and rotating it through 90 degree

--- src/component/ThreeScene.js
+++ src/component/ThreeScene.js
@@ -1,18 +1,23 @@
-import React from 'react'
-import { angleToRadians } from '../utils/angle'
+import React from "react";
+import { angleToRadians } from "../utils/angle";
 
 const ThreeScene = () => {
   return (
     //  Ball
+    <>
       <mesh>
-          <sphereGeometry args={[1,32,32] } />
-          <meshStandardMaterial color='#ffffff' />
+        <sphereGeometry args={[1, 32, 32]} />
+        <meshStandardMaterial color="#ffffff" />
+      </mesh>
 
+      {/* ambient light */}
+      <ambientLight args={["#ffffff", 1]}  />
+          <mesh rotation={[(angleToRadians(90)),0,0]}>
+        <planeGeometry args={[7, 7]} />
+        <meshStandardMaterial color="#1ea3d8" />
+      </mesh>
+    </>
+  );
+};
 
-          {/* ambient light */}
-          <ambientLight args={ ['#ffffff',1]} />
-    </mesh>
-  )
-}
-
-export default ThreeScene
+export default ThreeScene;
