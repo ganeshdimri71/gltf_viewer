78e06c6f2f6144591bf0d55d1fba027e80741a32
ganeshdimri71 <ganeshdimri71@gmail.com>
2022-07-28 21:26:59 +0530

    Setting the azimuthal angle to 90

--- src/component/ThreeScene.js
+++ src/component/ThreeScene.js
@@ -1,19 +1,40 @@
 import { PerspectiveCamera, OrbitControls } from "@react-three/drei";
-import React from "react";
+import { useFrame } from "@react-three/fiber";
+import React, { useEffect, useRef } from "react";
 import { angleToRadians } from "../utils/angle";
 
 const ThreeScene = () => {
+  const orbitControlsRef = useRef(null);
+  useFrame((state) => {
+    // console.log(state.mouse);
+    if (!!orbitControlsRef.current) {
+      const { x, y } = state.mouse;
+        orbitControlsRef.current.setAzimuthalAngle(-x *angleToRadians(90));
+        orbitControlsRef.current.update()
+    }
+  });
+
+  useEffect(() => {
+    if (!!orbitControlsRef.current) {
+      //   console.log(orbitControlsRef.current);
+    //   console.log(orbitControlsRef.current);
+    }
+  }, [orbitControlsRef.current]);
   return (
     <>
-          <PerspectiveCamera makeDefault position={[0, 1, 5]} />
-          <OrbitControls />
+      <PerspectiveCamera makeDefault position={[0, 1, 5]} />
+      <OrbitControls
+        ref={orbitControlsRef}
+        //   autoRotate={true}
+        // enableDamping={false}
+      />
       {/* Ball */}
-      <mesh position={[0,0.5,0]}>
+      <mesh position={[0, 0.5, 0]}>
         <sphereGeometry args={[0.5, 32, 32]} />
         <meshStandardMaterial color="#ffffff" />
       </mesh>
 
-      <mesh rotation={[-(angleToRadians(90)), 0, 0]}>
+      <mesh rotation={[-angleToRadians(90), 0, 0]}>
         <planeGeometry args={[7, 7]} />
         <meshStandardMaterial color="#1ea3d8" />
       </mesh>
