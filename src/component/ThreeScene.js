c77cc2a939f0184fa58ad111cdf7a6e887aff519
ganeshdimri71 <ganeshdimri71@gmail.com>
2022-07-28 20:06:08 +0530

    Moving the yaxis of the perspective camera

--- src/component/ThreeScene.js
+++ src/component/ThreeScene.js
@@ -1,21 +1,23 @@
+import { PerspectiveCamera } from "@react-three/drei";
 import React from "react";
 import { angleToRadians } from "../utils/angle";
 
 const ThreeScene = () => {
   return (
-    //  Ball
     <>
+      <PerspectiveCamera makeDefault position={[0, 1, 5]} />
+      {/* Ball */}
       <mesh>
-        <sphereGeometry args={[1, 32, 32]} />
+        <sphereGeometry args={[0.5, 32, 32]} />
         <meshStandardMaterial color="#ffffff" />
       </mesh>
 
-      {/* ambient light */}
-      <ambientLight args={["#ffffff", 1]}  />
-          <mesh rotation={[(angleToRadians(90)),0,0]}>
+      <mesh rotation={[-(angleToRadians(90)), 0, 0]}>
         <planeGeometry args={[7, 7]} />
         <meshStandardMaterial color="#1ea3d8" />
       </mesh>
+      {/* ambient light */}
+      <ambientLight args={["#ffffff", 1]} />
     </>
   );
 };
